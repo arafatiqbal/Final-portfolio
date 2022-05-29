232b0cb64be8da607bf2fc32a9f4bacd073b6994
arafatiqbal <arafatiqbal@gmail.com>
2022-05-29 02:50:25 -0700

    Fixing still

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const form = document.getElementById("form");
+const form = document.querySelector("#form");
 
 function sendMsg(e) {
-  const name = document.getElementById("name"),
-    subject = document.getElementById("subject"),
-    email = document.getElementById("email"),
-    message = document.getElementById("message");
+  const name = document.querySelector("#name"),
+    subject = document.querySelector("#subject"),
+    email = document.querySelector("#email"),
+    message = document.querySelector("#message");
 
   Email.send({
     SecureToken: "1844b681-b0c6-42e4-8d8d-c5d39430a725",
@@ -13,6 +13,7 @@ function sendMsg(e) {
     Subject: subject.value,
     Body: message.value,
   }).then((message) => alert(message));
+  console.log("inside");
 }
 
 form.addEventListener("submit", sendMsg);
