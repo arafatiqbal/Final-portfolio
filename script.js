934b4e949dc4ff549698b6fc13daf58e3af960b6
arafatiqbal <arafatiqbal@gmail.com>
2022-05-29 02:06:34 -0700

    Added javascript functionality to the form to send messages to my email

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-<script src="https://smtpjs.com/v3/smtp.js"></script>;
+const form = document.getElementById("form");
+
+function sendMsg(e) {
+  const name = document.getElementById("name"),
+    subject = document.getElementById("subject"),
+    email = document.getElementById("email"),
+    message = document.getElementById("message");
+
+  Email.send({
+    SecureToken: "1844b681-b0c6-42e4-8d8d-c5d39430a725",
+    To: "contact.arafatiq@gmail.com",
+    From: email.value,
+    Subject: subject,
+    Body: message,
+  }).then((message) => alert(message));
+}
+
+form.addEventListener("submit", sendMsg);
