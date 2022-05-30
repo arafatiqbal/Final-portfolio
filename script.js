9eb61e77277b209aa8d6725e213101b99b3515cf
arafatiqbal <arafatiqbal@gmail.com>
2022-05-29 23:41:09 -0700

    still fixing mail

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const name = document.querySelector("#name"),
 
 function sendEmail() {
   Email.send({
-    SecureToken: "bcd89087-d6ec-4cd0-bbc2-364b02e2de77",
-    To: "contact.arafatiq@gmail.com",
+    Host: "smtp.elasticemail.com",
+    Username: "arafatiqbal123@gmail.com",
+    Password: "D77D93E091EE432EABB3195A4E56491F7927",
+    To: "arafatiqbal123@gmail.com",
     From: email.value,
     Subject: subject.value,
     Body: message.value,
